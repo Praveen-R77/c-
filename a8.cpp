d83451026cb9fbe77ebb1502605b3978b37aaeca
Praveen-R77 <praveenrajamoorthy@gmail.com>
2024-06-20 21:27:59 +0530

    Docs cpp : Day3 int muliply

---
+++ a8.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int arr[] ={12,23,34,45};
+  
+  for(int ele : arr)
+   cout << ele << endl;
+   cout << endl;
+   
+   std::multiply2;
+   auto res = std::for_each(begin(arr),end(arr),multiplyby2 );
+   
+   for(int ele : arr )
+   
+   cout << ele << endl;
+   return 0;
+   }
