abf6ffd7c649c74203a666001d04ea49534bf0c0
Praveen-R77 <praveenrajamoorthy@gmail.com>
2024-06-20 21:24:05 +0530

    Docs cpp : Day3 string count

---
+++ a3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+  string str = "Sikander;
+  char key; 
+  int counts = 0;
+  
+  for(int i=0 ; i < sizeoof(str) ; i++){
+  
+  int n_occ = cout(begin(str),end(str),str[i]);
+  
+  if(n_occ > count)
+  {
+    counts = n_occ;
+    key =str[i];
+    }
+    }
+    
+    cout<<"counts - "<< count << "key - " << key <<endl;
+    
+    return 0;
+    
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+  
