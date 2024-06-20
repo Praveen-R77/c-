e21be1afb39bf742cb84677f750079e9ef19dc66
Praveen-R77 <praveenrajamoorthy@gmail.com>
2024-06-20 14:52:17 +0530

    Docs cpp:Day2 swappin two array

---
+++ p25.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+  int a = 10 ;
+  int b = 20;
+  cout << a << " " << b << endl;
+  swap(a,b);
+  cout << a << " " << b << endl;
+  }
