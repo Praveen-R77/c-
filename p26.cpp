8171cd40f2229878982eecf33083d2594a07097c
Praveen-R77 <praveenrajamoorthy@gmail.com>
2024-06-20 14:54:49 +0530

    Docs cpp:Day2 str and int

---
+++ p26.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+int main()
+{
+ int a = 5;
+ int b = 10;
+ std::cout << std::min(a,b) << std::endl;
+ 
+ std::string s1 = "Tamil Nadu", s2 = "Kerala";
+ std::cout << std::min(s1 , s2) << std::endl;
+ }
