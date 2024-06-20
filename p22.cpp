135072a45cc671c082f89b6e3842da06a02ef452
Praveen-R77 <praveenrajamoorthy@gmail.com>
2024-06-20 14:41:37 +0530

    Docs cpp:Day2 const int

---
+++ p22.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   auto x=5;
+   
+   const auto y= 10;
+   y = 20;
+   
+   const auto& crl = x;
+   cr1 =30;
+   
+   auto  &cr2 = y;
+   cr2 = 30;
+   return 0;
+   }
